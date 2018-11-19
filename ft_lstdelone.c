46b0fb0b97b5a297ce9c1cd2dba2dab64643fadd
alamit <adrien.lamit@effixis.ch>
2018-11-19 15:36:43 -0800

    lstnew

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/19 14:56:05 by alamit            #+#    #+#             */
-/*   Updated: 2018/11/19 15:38:05 by alamit           ###   ########.fr       */
+/*   Created: 2018/11/19 15:36:59 by alamit            #+#    #+#             */
+/*   Updated: 2018/11/19 15:37:54 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-t_list	*ft_lstnew(const void *content, size_t content_size)
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list	*res;
 
-	if (content)
-	{
-		res->content = ft_memdup(content, content_size);
-		res->content_size = content_size;
-	}
-	else
-	{
-		res->content = NULL;
-		res->content_size = 0;
-	}
-	res->next = NULL;
-	return (res);
 }
