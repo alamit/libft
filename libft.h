163dee11131e262ddc70d5ef960ad35239e3c3a8
alamit <adrien.lamit@effixis.ch>
2018-11-19 16:08:36 -0800

    lstadd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 16:07:34 by alamit            #+#    #+#             */
-/*   Updated: 2018/11/19 15:09:33 by alamit           ###   ########.fr       */
+/*   Updated: 2018/11/19 16:05:33 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ t_list			*ft_lstnew(const void *content, size_t content_size);
 void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
-void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void			ft_lstiter(t_list *lst, void (*f)(t_list *));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *));
 
 #endif
