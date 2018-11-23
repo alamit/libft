d7216c9dbda8982246658977328b59a888d34723
alamit <adrien.lamit@effixis.ch>
2018-11-23 11:09:35 -0800

    protect lstmap against user function failure

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 17:49:13 by alamit            #+#    #+#             */
-/*   Updated: 2018/11/23 10:25:15 by alamit           ###   ########.fr       */
+/*   Updated: 2018/11/23 11:09:07 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,17 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *))
 	if (lst == NULL || f == NULL)
 		return (NULL);
 	new = ft_lstnew(lst->content, lst->content_size);
-	if (new == NULL)
+	if (new == NULL || (new = f(new)) == NULL)
 		return (NULL);
-	new = f(new);
 	res = new;
 	while ((lst = lst->next))
 	{
 		new->next = ft_lstnew(lst->content, lst->content_size);
-		if (new->next == NULL)
+		if (new->next == NULL || (new->next = f(new->next)) == NULL)
 		{
 			ft_lstdel(&res, NULL);
 			return (NULL);
 		}
-		new->next = f(new->next);
 		new = new->next;
 	}
 	return (res);
