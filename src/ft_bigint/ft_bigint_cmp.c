b9e48ffbe1fdb762f5e8c89290f167325767e0a1
alamit <adrien.lamit@effixis.ch>
2019-03-25 17:22:19 +0100

    fixed comparison when difference is in index 0

--- src/ft_bigint/ft_bigint_cmp.c
+++ src/ft_bigint/ft_bigint_cmp.c
@@ -6,7 +6,7 @@
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 06:10:00 by alamit            #+#    #+#             */
-/*   Updated: 2019/03/21 14:19:41 by alamit           ###   ########.fr       */
+/*   Updated: 2019/03/25 17:21:26 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_bigint_cmp(const t_bigint *a, const t_bigint *b)
 	if (a->size != b->size)
 		return (b->size > a->size ? 1 : -1);
 	i = a->size;
-	while (--i)
+	while (i--)
 	{
 		if (a->arr[i] != b->arr[i])
 			return (b->arr[i] > a->arr[i] ? 1 : -1);
