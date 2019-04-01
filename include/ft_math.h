3193275f815698eee76177a292a76b936503d8b0
alamit <adrien.lamit@effixis.ch>
2019-04-01 12:02:32 +0200

    refactored abs function and added log10

--- include/ft_math.h
+++ include/ft_math.h
@@ -6,17 +6,21 @@
 /*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 20:07:18 by alamit            #+#    #+#             */
-/*   Updated: 2019/03/20 20:07:46 by alamit           ###   ########.fr       */
+/*   Updated: 2019/03/28 14:12:50 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATH_H
 # define FT_MATH_H
 
+# include <inttypes.h>
+# include <sys/types.h>
+
 unsigned int	ft_isqrt(unsigned int n);
 unsigned int	ft_lowersqrt(unsigned int n);
 int				ft_ipow(int n, unsigned int exp);
 int				ft_isprime(unsigned int n);
-unsigned int	ft_abs(int n);
+uint64_t		ft_abs(int64_t n);
+size_t			ft_log10(uint64_t n);
 
 #endif
