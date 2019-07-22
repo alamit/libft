24e8fa8aa6fbd7d86d896134d224867e35cfbb5d
alamit <alamit@student.42.us.org>
2019-07-22 15:01:55 +0200

    fix error when sprintf with stack pointer

--- src/ft_buff/ft_buff_init.c
+++ src/ft_buff/ft_buff_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_buff_init.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alamit <alamit@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alamit <alamit@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 17:19:09 by alamit            #+#    #+#             */
-/*   Updated: 2019/03/28 21:25:09 by alamit           ###   ########.fr       */
+/*   Updated: 2019/07/22 13:40:28 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_buff_init(t_buff *buff, int fd, char **strp)
 {
 	buff->idx = 0;
 	buff->fd = fd;
+	if (strp)
+		*strp = NULL;
 	buff->strp = strp;
 	buff->written = 0;
 }
