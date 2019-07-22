4902e169de4ae133c7f14d43c86266ff1a69b017
alamit <alamit@student.42.us.org>
2019-07-22 15:09:40 +0200

    add ft_printf header for eval

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,15 +6,13 @@
 /*   By: alamit <alamit@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 20:21:09 by alamit            #+#    #+#             */
-/*   Updated: 2019/02/26 15:39:02 by alamit           ###   ########.fr       */
+/*   Updated: 2019/07/22 15:08:06 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int		ft_sprintf(char **str, const char *format, ...);
-int		ft_dprintf(int fd, const char *format, ...);
-int		ft_printf(const char *format, ...);
+# include <ft_stdio.h>
 
 #endif
